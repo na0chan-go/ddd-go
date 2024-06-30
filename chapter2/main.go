0b8fa998b33a0c00937d85d980a2cf69762bfb28
na0chan-go <nn19960218@gmail.com>
2024-06-30 17:42:32 +0900

    feat: 値オブジェクト化

--- chapter2/main.go
+++ chapter2/main.go
@@ -2,12 +2,37 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
 func main() {
-	fullName := "John Doe"
-	token := strings.Split(fullName, " ")
-	firstName := token[0]
-	fmt.Println(firstName) // John
+	fullName := newFullName("John", "Doe")
+	fmt.Println(fullName.firstName) // John
+	fmt.Println(fullName.lastName)  // Doe
+
+	fmt.Println(fullName.FirstName()) // John
+	fmt.Println(fullName.LastName())  // Doe
+}
+
+// fullName struct
+type fullName struct {
+	firstName string
+	lastName  string
+}
+
+// newFullName constructor
+func newFullName(firstName, lastName string) *fullName {
+	return &fullName{
+		firstName: firstName,
+		lastName:  lastName,
+	}
+}
+
+// FirstName getter
+func (f *fullName) FirstName() string {
+	return f.firstName
+}
+
+// LastName getter
+func (f *fullName) LastName() string {
+	return f.lastName
 }
